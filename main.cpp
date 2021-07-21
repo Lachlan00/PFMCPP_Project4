b2f25babd846981d5fd6d3cf57e02c38b7a769bf
Lachlan <lachlan.phillips.00@gmail.com>
2021-07-21 07:11:39 +0000

    Fixed warning for float to double conversion

--- main.cpp
+++ main.cpp
@@ -92,7 +92,7 @@ struct DoubleType
     // divide
     double divide(double lhs, double rhs)
     {
-        if (rhs == 0.0f)
+        if (rhs == 0.0)
         {
             std::cout << "warning, floating point division by zero returns 'inf' !" << std::endl;
         }
